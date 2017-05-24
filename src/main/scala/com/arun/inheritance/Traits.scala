c472c4404957e91fd887d2d5422b57e4f0dcf9fc
strider90arun <strider90arun@gmail.com>
2017-05-23 23:44:49 -0700

    Scala traits

---
+++ src/main/scala/com/arun/inheritance/Traits.scala
@@ -0,0 +1,19 @@
+package com.arun.inheritance
+
+trait Pet {
+  def speak { println("Yo") }
+  def comeToMaster
+}
+
+class Dog extends Pet {
+  def comeToMaster { ("I'm coming!") }
+}
+
+class Cat extends Pet {
+  override def speak { ("meow") }
+  def comeToMaster { ("That's not gonna happen.") }
+}
+
+class Traits {
+
+}
